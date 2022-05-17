12e5d2b9ed76f9159da962114b0e7e4fa5a73391
TaneriX07 <tanerivince@gmail.com>
2022-05-17 11:06:45 +0800

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Board from './pages/Board/Board';
 import Backlog from './pages/Backlog/Backlog';
 import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Board />
-      <Backlog />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Board />} />
+          <Route path="/backlog" element={<Backlog />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
-import './Header.css'
+import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="Header-wrapper">
       <h1>Project Management Board</h1>
+      <nav className="Navigation-wrapper">
+        <Link className="App-link" to="/">
+          Home
+        </Link>
+        <Link className="App-link" to="/backlog">
+          Backlog
+        </Link>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Header;
