9d3167d3c0517b7b5bd5a8057782d48a48efe332
TaneriX07 <tanerivince@gmail.com>
2022-05-17 10:01:17 +0800

    Add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+A project management board PWA that used custom hooks for data fetching and HTML5 Drag and Drop API for dynamic interaction.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
 import './App.css';
 import Board from './pages/Board/Board';
 import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       <Board />
     </div>
